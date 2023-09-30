50719a8a28a03f2fb19eed3c84458fae72745014
Deimer Navarro <deimerjoe@gmail.com>
2023-09-30 10:37:12 -0500

    front: created breadcrumb

--- frontend/src/app/modules/home/home.module.ts
+++ frontend/src/app/modules/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    BreadcrumbsComponent,
   ],
   imports: [
     CommonModule,
